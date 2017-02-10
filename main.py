db1b996709d32bf7a263c2ea460d6d13fbbc8f3e
ushills <web@ushills.co.uk>
2017-02-10 15:46:36 +0000

    added comments and equal to for date cut off

--- main.py
+++ main.py
@@ -4,32 +4,41 @@ from datetime import datetime
 DATE_FORMAT = "%d/%m/%Y"
 
 def main():
+    # get the cut off date for transactions that
+    # we want to exclude
     cutoffdate = dateinput()
     newrowslist = []
+
+    # Open the csv file, check the dates are valid,
+    # reduce the name and recombine to the newrowslist
+    # variable to be written as a new csv file
     with open('TransHist.csv') as csvfile:
         transreader = csv.reader(csvfile, delimiter=',', quotechar='"')
         try:
             for row in transreader:
-                # print (row[0])
+                # get the date from the row
                 date = checkdate(row)
-                # print ('Date =', date)
-                # print ('Cutoff date =', cutoffdate)
-                if date > cutoffdate:
-                    # print ('Date ok...appending')
+                # if the date is greater than the cutoffdate
+                # run the stripname function and add to the
+                # newrowslist
+                if date >= cutoffdate:
                     newrowslist.append(stripname(row))
                 else:
-                    # print ('Date too early...skipping')
                     pass
         except:
             pass
-    print (newrowslist)
+
+    # now we have the newrowslist, write this to a new csv file
     writecsv(newrowslist)
 
 def stripname(inputrow):
     try:
+        # extract the name from the row
         name = inputrow[1]
+        # strip the extra information
         name = name.split("  ")
         name = name[0]
+        # recombine to create a new correct row
         newrow = [inputrow[0], name, inputrow[2]]
         return newrow
     except:
@@ -37,18 +46,17 @@ def stripname(inputrow):
 
 def checkdate(inputrow):
     try:
-        # print ('Running checkdate')
+        # extract the date information from the row and
+        # convert it to datetime format
         d = inputrow[0]
         d = datetime.strptime(d, DATE_FORMAT)
         d = datetime.date(d)
-        print ('Checkdate return =', d)
         return d
     except:
-        print ('Checkdate failed')
         pass
 
 def dateinput():
-    d = input('Cut off date for transactions (d/m/YYYY)>')
+    d = input('Enter the cut off date for transactions (d/m/YYYY)> ')
     try:
         d = datetime.strptime(d, DATE_FORMAT)
         d = datetime.date(d)
@@ -57,11 +65,12 @@ def dateinput():
         print ('Incorrect format...try again')
 
 def writecsv(newrowslist):
+    # write the newrowslist to the csv file
+    # TransHistNew.csv
     with open('TransHistNew.csv', 'w', newline='') as csvfile:
         transwriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
         transwriter.writerows(newrowslist)
-
 
 
 main()
